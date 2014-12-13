ffd4e7f1baed383e183443ae023b187767249efe
hayajo <hayajo@cpan.org>
2014-12-13 12:42:43 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::MyApp - It's new $module
+
+# SYNOPSIS
+
+    use App::MyApp;
+
+# DESCRIPTION
+
+App::MyApp is ...
+
+# LICENSE
+
+Copyright (C) hayajo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hayajo <>
---
+++ lib/App/MyApp.pm
@@ -0,0 +1,39 @@
+package App::MyApp;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::MyApp - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::MyApp;
+
+=head1 DESCRIPTION
+
+App::MyApp is ...
+
+=head1 LICENSE
+
+Copyright (C) hayajo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hayajo E<lt>E<gt>
+
+=cut
+
